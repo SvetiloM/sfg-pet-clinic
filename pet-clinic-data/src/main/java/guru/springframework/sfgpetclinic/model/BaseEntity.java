2ce879be77a9afafd2768bb06631bb1f5db27530
svetlanamorgunova <smorgunova000@gmail.ru>
2023-05-21 20:15:18 +0400

    add Hbernate MappingSuperclass to BaseEntity

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -1,8 +1,13 @@
 package guru.springframework.sfgpetclinic.model;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
